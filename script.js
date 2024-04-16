18374a10872ebaae525419112e433a7c2f36d10f
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-04-16 16:39:52 +0300

    basic project JavaScript functionality

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 // console.log("Etch a sketch");
+
+const GRID = 721;
+let squaresPerSide= 19;
+
+const sketchArea = document.querySelector("#sketch-area");
+const sliderContainer = document.querySelector("#slider-container");
+const slider = document.querySelector("#slider");
+const sliderValue = document.querySelector("#slider-value");
+
+sliderValue.textContent = `${slider.value} times ${slider.value} (Resolution)`;
+
+sketchArea.style.width = sketchArea.style.height = `${GRID}px`;
+
+function changeBackgroundColor(){
+    this.style.backgroundColor = "#daa520";
+}
+
+function createGridCells() {
+    const numberOfSquares = (squaresPerSide * squaresPerSide);
+    const widthOrHeight = `${(GRID / squaresPerSide) - 2}px`
+
+    for (let i = 0;i < numberOfSquares; i++){
+        const singleGridCell = document.createElement("div");
+
+        singleGridCell.style.width = singleGridCell.style.height = widthOrHeight;
+
+        singleGridCell.classList.add("cell");
+
+        sketchArea.appendChild(singleGridCell);
+        
+        singleGridCell.addEventListener("mouseover", changeBackgroundColor);
+    }
+}
+
+function cleanGridCells() {
+    while (sketchArea.firstChild) {
+        sketchArea.removeChild(sketchArea.firstChild);
+    }
+}
+
+slider.oninput = function() {
+    let txt = `${this.value} times ${this.value} (Resolution)`;
+    sliderValue.innerHTML = txt;
+    cleanGridCells();
+    createGridCells(this.value);
+}
+
+createGridCells(19);
